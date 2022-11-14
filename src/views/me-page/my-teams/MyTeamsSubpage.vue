9d8a2d3471288a8c474186c12e32c03f4952868a
bendik0329 <razvanbendik0329@gmail.com>
2022-11-14 10:15:31 -0600

    Update MyTeamsSubpage.vue

--- src/views/me-page/my-teams/MyTeamsSubpage.vue
+++ src/views/me-page/my-teams/MyTeamsSubpage.vue
@@ -1,3 +1,183 @@
 <template>
-    <h2>MyTeamsSubpage</h2>
+ 
+
+    <!-- blogs -->
+    <b-row class="blog-list-wrapper">
+      <b-col
+        v-for="blog in blogList"
+        :key="blog.img"
+        md="6"
+      >
+        <b-card
+          tag="article"
+          no-body
+        >
+          <b-link :to="{ name: 'pages-blog-detail', params: { id: blog.id } }">
+            <b-img
+              :src="blog.img"
+              :alt="blog.img.slice(5)"
+              class="card-img-top"
+            />
+          </b-link>
+          <b-card-body>
+            <b-card-title>
+              <b-link
+                :to="{ name: 'pages-blog-detail', params: { id: blog.id } }"
+                class="blog-title-truncate text-body-heading"
+              >
+                {{ blog.title }}
+              </b-link>
+            </b-card-title>
+            <b-media no-body>
+              <b-media-aside
+                vertical-align="center"
+                class="mr-50"
+              >
+                <b-avatar
+                  href="javascript:void(0)"
+                  size="24"
+                  :src="blog.avatar"
+                />
+              </b-media-aside>
+              <b-media-body>
+                <small class="text-muted mr-50">by</small>
+                <small>
+                  <b-link class="text-body">{{ blog.userFullName }}</b-link>
+                </small>
+                <span class="text-muted ml-75 mr-50">|</span>
+                <small class="text-muted">{{ blog.blogPosted }}</small>
+              </b-media-body>
+            </b-media>
+            <div class="my-1 py-25">
+              <b-link
+                v-for="(tag,index) in blog.tags"
+                :key="index"
+              >
+                <b-badge
+                  pill
+                  class="mr-75"
+                  :variant="tagsColor(tag)"
+                >
+                  {{ tag }}
+                </b-badge>
+              </b-link>
+            </div>
+            <b-card-text class="blog-content-truncate">
+              {{ blog.excerpt }}
+            </b-card-text>
+            <hr>
+            <div class="d-flex justify-content-between align-items-center">
+              <b-link :to="{ path: `/pages/blog/${blog.id}#blogComment`}">
+                <div class="d-flex align-items-center text-body">
+                  <feather-icon
+                    icon="MessageSquareIcon"
+                    class="mr-50"
+                  />
+                  <span class="font-weight-bold">{{ kFormatter(blog.comment) }} Comments</span>
+                </div>
+              </b-link>
+              <b-link
+                :to="{ name: 'pages-blog-detail', params: { id: blog.id } }"
+                class="font-weight-bold"
+              >
+                Read More
+              </b-link>
+            </div>
+          </b-card-body>
+        </b-card>
+      </b-col>
+      <b-col cols="12">
+        <!-- pagination -->
+        <div class="my-2">
+          <b-pagination
+            v-model="currentPage"
+            align="center"
+            :total-rows="rows"
+            first-number
+            last-number
+            prev-class="prev-item"
+            next-class="next-item"
+          >
+            <template #prev-text>
+              <feather-icon
+                icon="ChevronLeftIcon"
+                size="18"
+              />
+            </template>
+            <template #next-text>
+              <feather-icon
+                icon="ChevronRightIcon"
+                size="18"
+              />
+            </template>
+          </b-pagination>
+        </div>
+      </b-col>
+    </b-row>
+
+    <!--/ blogs -->
+
+
+
 </template>
+
+<script>
+import {
+  BRow, BCol, BCard, BFormInput, BCardText, BCardTitle, BMedia, BAvatar, BMediaAside, BMediaBody, BImg, BCardBody, BLink, BBadge, BFormGroup, BInputGroup, BInputGroupAppend, BPagination,
+} from 'bootstrap-vue'
+import { kFormatter } from '@core/utils/filter'
+import ContentWithSidebar from '@core/layouts/components/content-with-sidebar/ContentWithSidebar.vue'
+
+export default {
+  components: {
+    BRow,
+    BCol,
+    BCard,
+    BFormInput,
+    BCardText,
+    BCardBody,
+    BCardTitle,
+    BMedia,
+    BAvatar,
+    BMediaAside,
+    BMediaBody,
+    BLink,
+    BBadge,
+    BFormGroup,
+    BInputGroup,
+    BInputGroupAppend,
+    BImg,
+    BPagination,
+    ContentWithSidebar,
+  },
+  data() {
+    return {
+      search_query: '',
+      blogList: [],
+      blogSidebar: {},
+      currentPage: 1,
+      perPage: 1,
+      rows: 140,
+    }
+  },
+  created() {
+    this.$http.get('/blog/list/data').then(res => { this.blogList = res.data })
+    this.$http.get('/blog/list/data/sidebar').then(res => { this.blogSidebar = res.data })
+  },
+  methods: {
+    kFormatter,
+    tagsColor(tag) {
+      if (tag === 'Quote') return 'light-info'
+      if (tag === 'Gaming') return 'light-danger'
+      if (tag === 'Fashion') return 'light-primary'
+      if (tag === 'Video') return 'light-warning'
+      if (tag === 'Food') return 'light-success'
+      return 'light-primary'
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+@import '@core/scss/vue/pages/page-blog.scss';
+</style>
